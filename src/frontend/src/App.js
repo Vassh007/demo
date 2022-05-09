43b0a659dbef4562161bf8610144d9bd4bb890f1
Suvash Shrestha <79300198+Vassh007@users.noreply.github.com>
2022-05-09 01:20:35 -0400

    Update App.js

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import {  
+        Button, 
+        Radio,
+        RadioGroup,
+        FormControl,
+        FormControlLabel,
+        FormLabel
+
+  } 
+from '@mui/material';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Vutton"><br></br><br></br>
+        <Button id='Primary' type='primary' size= "large" variant="contained">
+          Hello
+        </Button>
+        <div>
+          <FormControl>
+            <FormLabel id="demo-row-radio-buttons-group-label" className='School'></FormLabel>
+            <RadioGroup
+              row
+              aria-labelledby="demo-row-radio-buttons-group-label"
+              name="row-radio-buttons-group"
+            >
+              <FormControlLabel value="Students" control={<Radio />} label="Students" className='Button1'/>
+              <FormControlLabel value="Teachers" control={<Radio />} label="Teacher" className='Button2' />
+              <FormControlLabel value="Staffs" control={<Radio />} label="Other" className='Button3' />
+              </RadioGroup>
+          </FormControl>
+        </div>
+      </div>
     </div>
   );
 }
